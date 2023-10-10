82db3077331429909b17873c7ddb91a4751f5de2
Robert Brook <robertbrook@fastmail.fm>
2023-10-10 17:59:39 +0100

    line up puller

--- puller.pl
+++ puller.pl
@@ -1,3 +1,10 @@
 #!/usr/bin/env perl
 use Modern::Perl;
 use Mojolicious::Lite;
+
+use Mojo::File;
+
+# curl https://www.legislation.gov.uk/new/data.feed --output feed.rss
+
+my $path = Mojo::File->new('feed.rss');
+say $path->slurp;
