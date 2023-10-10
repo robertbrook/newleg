5c9e50301bef0914899a82be3a1779dda88d18f0
Robert Brook <robertbrook@fastmail.fm>
2023-10-10 22:04:39 +0100

    try spew

--- puller.pl
+++ puller.pl
@@ -4,37 +4,38 @@ use Mojolicious::Lite;
 use Mojo::File;
 use Mojo::DOM;
 use Mojo::Template;
+use Path::Tiny qw(path);
 
 # curl https://www.legislation.gov.uk/new/data.feed --output feed.rss
 
-my $path = Mojo::File->new('feed.rss');
-my $dom  = Mojo::DOM->new( $path->slurp );
-
-my $mt = Mojo::Template->new;
-say $mt->render(<<'MAIN');
+my $mt     = Mojo::Template->new;
+my $output = $mt->render(<<'MAIN');
+% my $path = Mojo::File->new('feed.rss');
+% my $dom  = Mojo::DOM->new( $path->slurp );
 % use Time::Piece;
 <div>
   % my $now = localtime;
   Time: <%= $now->hms %>
 </div>
+
+ % for my $entry ( $dom->find('entry')->each ) {
+    SELF <%= $entry->at("link[rel='self']")->attr('href') %>
+    DMT <%= $entry->at( 'ukm|DocumentMainType',
+        ukm => 'http://www.legislation.gov.uk/namespaces/metadata' )
+      ->attr('Value') %>
+      EN TITLE <%= $entry->at('title')->text %>
+      SUMMARY <%= $entry->at('summary')->text %>
+      
+    % if ( length $entry->at('category') ) {
+        CAT <%=  $entry->at('category')->attr('term') %>
+    % }
+    
+    UPDATED <%= $entry->at('updated')->text %>
+    PUBLISHED <%= $entry->at('published')->text %>
+    
+% }
+  
 MAIN
 
-for my $entry ( $dom->find('entry')->each ) {
+path('output.html')->spew($output);
 
-    #   say $entry->at("id")->text;
-    say $entry->at("link[rel='self']")->attr('href');
-    say $entry->at( 'ukm|DocumentMainType',
-        ukm => 'http://www.legislation.gov.uk/namespaces/metadata' )
-      ->attr('Value');
-
-    #   say $entry->at('title')->text; # check for welsh
-    #   say $entry->at('summary')->text;
-
-    if ( length $entry->at('category') ) {
-        say $entry->at('category')->attr('term');
-    }
-
-    #   say $entry->at('updated')->text;
-    #   say $entry->at('published')->text;
-    say "";
-}
